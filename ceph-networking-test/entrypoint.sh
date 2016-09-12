830d789382d2052e94c9eb2be509ec16bc49335f
Patrick Christopher <coffeepac@gmail.com>
2016-09-12 14:29:16 -0700

    times re-arranging!

--- ceph-networking-test/entrypoint.sh
+++ ceph-networking-test/entrypoint.sh
@@ -3,8 +3,9 @@ set -ex
 
 NIC_MORE_TRAFFIC=$(grep -vE "lo:|face|Inter" /proc/net/dev | sort -n -k 2 | tail -1 | awk '{ sub (":", "", $1); print $1 }')
 if command -v ip; then
-  if [ 1 -eq 1 ]; then
+  if [ ${NETWORK_AUTO_DETECT} -eq 1 ]; then
     MON_IP=$(ip -6 -o a s $NIC_MORE_TRAFFIC | awk '{ sub ("/..", "", $4); print $4 }')
+    CEPH_PUBLIC_NETWORK=$(ip -6 r | grep $NIC_MORE_TRAFFIC | awk '{ print $1 }')
     if [ -z "$MON_IP" ]; then
       MON_IP=$(ip -4 -o a s $NIC_MORE_TRAFFIC | awk '{ sub ("/..", "", $4); print $4 }')
       CEPH_PUBLIC_NETWORK=$(ip r | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/[0-9]\{1,2\}' | head -1)
@@ -25,5 +26,9 @@ else
   CEPH_PUBLIC_NETWORK=$(grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/[0-9]\{1,2\}' /proc/net/fib_trie | grep -vE "^127|^0" | head -1)
 fi
 
+if [[ -z "$MON_IP" || -z "$CEPH_PUBLIC_NETWORK" ]]; then
+  echo "ERROR- it looks like we have not been able to discover the network settings"
+  exit 1
+fi
 
 sleep 300
